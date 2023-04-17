7a78910a2a3119ff977cb5c01c010a8286fc851d
Aquiles-bb <brunoaquiles@ufpr.br>
2023-04-17 09:06:01 -0300

    Initial commit

---
+++ crud.py
@@ -0,0 +1,13 @@
+import mysql.connector 
+
+conexao = mysql.connector.connect (
+    host = 'localhost',
+    user = 'root',
+    password = 'root',
+    database = 'bdteste',
+)
+
+cursor = conexao.cursor()
+
+cursor.close()
+conexao.close()
